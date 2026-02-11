fae2f3a1287738a5f69c75a744e2094130688775
Michael Bui <lovethebookofmormon@gmail.com>
2026-02-11 08:19:28 -0700

    fixed workstation

--- src/components/WorkStation.tsx
+++ src/components/WorkStation.tsx
@@ -1,6 +1,6 @@
 export default function WorkStation() {
 	return (
-		<div className='mx-auto mt-5 h-fit w-full bg-background md:mb-20 md:w-[577px]'>
+		<div className='mx-auto mt-5 h-fit w-full bg-background md:mt-0 md:mb-20 md:w-[577px]'>
 			<h3 className='text-left font-bold font-sans text-base text-foreground'>Item Name</h3>
 			<p className='mt-4 text-left font-normal font-sans text-base text-foreground leading-[28px] tracking-normal md:leading-7'>
 				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et massa mi. Aliquam in hendrerit urna. Pellentesque sit
